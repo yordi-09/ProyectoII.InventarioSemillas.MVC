b189f5b18e5d7f2962f143a93be3764f86e3ecf7
Yordi Elizondo Montes <yelizondo@datasoft.co.cr>
2025-07-25 12:10:10 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoII.InventarioSemillas.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoII.InventarioSemillas.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
